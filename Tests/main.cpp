348b9b2f2cd3a4817f9b15c9223bc6db9e5394b1
MurylloEx <32225687+MurylloEx@users.noreply.github.com>
2024-07-07 22:54:23 -0300

    test: configure command line testing host entrypoint

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -1,8 +1,5 @@
-#include <gtest/gtest.h>
+#include <catch2/catch_session.hpp>
 
-
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char* argv[]) {
+	return Catch::Session().run(argc, argv);
 }
