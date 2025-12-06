f24773500e310d3c7f1bb349b719f390a619f329
MurylloEx <32225687+MurylloEx@users.noreply.github.com>
2025-12-06 14:49:42 -0300

    feat: create __zyntercept_noinline macro for multiple compilers

--- Zyntercept/Core/Common/Common.h
+++ Zyntercept/Core/Common/Common.h
@@ -5,6 +5,21 @@
 #include <cstring>
 #include <algorithm>
 
+#if defined(_MSC_VER) || defined(__BORLANDC__) || defined(__CODEGEARC__) || defined(__DMC__)
+// MSVC, Borland, Digital Mars and compatibles with __declspec
+#define __zyntercept_noinline __declspec(noinline)
+#elif defined(__GNUC__) || defined(__clang__) || defined(__INTEL_COMPILER) || \
+      defined(__SUNPRO_C) || defined(__SUNPRO_CC) || defined(__xlC__) || \
+      defined(__IBMC__) || defined(__IBMCPP__) || defined(__HP_cc) || \
+      defined(__HP_aCC) || defined(__ARMCC_VERSION) || defined(__TI_COMPILER_VERSION__) || \
+      defined(__NVCC__) || defined(__PGI) || defined(__EMSCRIPTEN__)
+// GCC, Clang, Intel, Sun, IBM, HP, ARM, TI, NVIDIA, PGI, Emscripten
+#define __zyntercept_noinline __attribute__((noinline))
+#else
+#define __zyntercept_noinline __attribute__((noinline))
+#pragma message("Warning: Compiler not recognized. __zyntercept_noinline may not work properly.")
+#endif
+
 #define __zyntercept_cdecl
 #define __zyntercept_in
 #define __zyntercept_out
